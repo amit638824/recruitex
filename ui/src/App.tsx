c108d3bdf48989bd04d3754d7094a2294e0bc017
amit6388 <amit.chauhan@techwagger.com>
2026-08-22 21:48:11 +0530

    new

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,11 +2,19 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import { Routes, Route } from 'react-router-dom'
+import Home from './components/landing/Home';
+import FindJob from './components/landing/FindJob';
+import About from './components/landing/About';
+import Contact from './components/landing/Contact';
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<>home</>} /> 
+        {/* landing page */}
+        <Route path='/' element={<Home />} />
+        <Route path='/job' element={<FindJob />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} /> 
       </Routes>
     </>
   )

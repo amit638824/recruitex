5784fe617bf7beeb14aea40150a9ed81fa3553cf
amit6388 <amit.chauhan@techwagger.com>
2026-08-22 21:17:43 +0530

    new

---
+++ ui/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css' ;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+function App() { 
+  return (
+    <>
+    <h1 className='text-danger text-center'>Welcome TO Recruitex...</h1>
+    </>
+  )
+}
+
+export default App
+ 

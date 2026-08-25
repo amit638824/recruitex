11cdb626ec3414ccccc7374e1ecab8d2fae33229
amit6388 <amit.chauhan@techwagger.com>
2026-08-25 22:09:15 +0530

    new\

--- api/src/controller/controller.ts
+++ api/src/controller/controller.ts
@@ -1,6 +1,7 @@
 import { UserTbl } from "../entities/user";
 import { createResponse } from "../helper/utils";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 export const createUser = async (req: any, res: any) => {
     try {
         const { type, name, email, contact, password, location, qualification } = req.body;
@@ -21,7 +22,18 @@ export const createUser = async (req: any, res: any) => {
 export const LoginUser = async (req: any, res: any) => {
     try {
         const { email, password } = req.body;
-
+        const isExist = await UserTbl.findOne({ where: { email } })
+        if (!isExist) {
+            return createResponse(res, 400, false, "User not found", [])
+        } else {
+            const isMatch = await bcrypt.compare(password, isExist?.password)
+            if (!isMatch) {
+                return createResponse(res, 400, false, "Invalid password", [])
+            } else {
+                const token = await jwt.sign({ _id: isExist?.id, email: isExist?.email }, `${process.env?.JWT_SCRET}`, { expiresIn: "1d" })
+                return createResponse(res, 200, true, "User  login   successfully", { ...isExist, token })
+            }
+        }
     } catch (err) {
         return createResponse(res, 500, false, "Internal Server error", [])
     }
